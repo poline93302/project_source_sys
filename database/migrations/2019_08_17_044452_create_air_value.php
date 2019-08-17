4c16243bd643075359c0644d1de47035fd1852bf
yinglu_chen <poline93302>
2019-08-17 13:04:24 +0800

    連接資料庫與設定migrate

--- database/migrations/2019_08_17_044452_create_air_value.php
+++ database/migrations/2019_08_17_044452_create_air_value.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateAirValue extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('air_value', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('form_name');
-            $table->string('form_address');
-            $table->string('email');
-            $table->string('password');
-            $table->timestamps();
+            $table->string('come_form');
+            $table->string('sensor');
+            $table->double('value');
+            $table->dateTime('send_time');
         });
     }
 
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('air_value');
     }
 }
